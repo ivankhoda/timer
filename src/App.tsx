a9c29e62f1ef9830087b07b6343bf981754b74d1
Ivan Khoda <ivankhoda@gmail.com>
2021-10-09 23:02:15 +0300

    initial upload

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+function App() {
+  return <h1>Batut rabotaet</h1>;
+}
+
+export default App;
---
+++ webpack.config.ts
@@ -0,0 +1,44 @@
+import ForkTsCheckerWebpackPlugin from "fork-ts-checker-webpack-plugin";
+import path from "path";
+import { Configuration } from "webpack";
+const config: Configuration = {
+  entry: "./src/index.tsx",
+
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react", "@babel/preset-typescript"],
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "bundle.js",
+  },
+  devServer: {
+    static: path.join(__dirname, "build"),
+    compress: true,
+    port: 4000,
+  },
+  mode: "development",
+  plugins: [
+    new ForkTsCheckerWebpackPlugin({
+      async: false,
+      eslint: {
+        files: "./src/**/*",
+      },
+    }),
+  ],
+};
+
+export default config;
