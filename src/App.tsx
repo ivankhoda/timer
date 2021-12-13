f692496e7e04157a7060e2962107862a16575d89
Ivan Khoda <ivankhoda@gmail.com>
2021-12-13 22:31:30 +0300

    Small refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import React from "react";
 import { Routes } from "react-router";
 import { HashRouter, Route } from "react-router-dom";
@@ -6,10 +5,7 @@ import { SettingsPage } from "./components/SettingsPage/SettingsPage";
 import { Timer } from "./components/Timer/Timer";
 import "./style.scss";
 
-//TODO:fix type of App props, or refactor component
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const App = (props: any) => {
+export const App = () => {
   const routes = [
     { path: "/", name: "Timer", Component: <Timer /> },
     { path: "/settings", name: "Settings", Component: <SettingsPage /> },
